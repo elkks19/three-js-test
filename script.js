32fb55c657925ab9242f952547e8f571dd992c92
elkks19 <rafafabiani1909@gmail.com>
2025-02-19 14:48:18 -0400

    cambios

--- script.js
+++ script.js
@@ -12,11 +12,27 @@ const cilindro = new THREE.Mesh(geometria, material);
 escena.add(cilindro);
 
 // Crear una esfera
-const geometriaEsfera = new THREE.SphereGeometry( 1, 16, 8 ); 
+const geometriaEsfera = new THREE.SphereGeometry(1, 16, 8); 
 const materialEsfera = new THREE.MeshBasicMaterial({ color: 0xffff00, wireframe: true });
-const esfera = new THREE.Mesh( geometriaEsfera, materialEsfera );
+const esfera = new THREE.Mesh(geometriaEsfera, materialEsfera);
 escena.add(esfera);
 
+// Cargar textura de fondo
+const texture = new THREE.TextureLoader().load(`gato.jpg`);
+// Crear la malla de fondo
+const backgroundMesh = new THREE.Mesh(
+	new THREE.PlaneGeometry(2, 2),
+	new THREE.MeshBasicMaterial({
+		map: texture,
+		depthTest: false,
+		depthWrite: false
+	})
+);
+
+// La escena de fondo
+const backgroundScene = new THREE.Scene();
+const backgroundCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
+backgroundScene.add(backgroundMesh);
 
 // Posicionar el cilindro al lado derecho
 cilindro.position.x = 2;
@@ -26,7 +42,7 @@ esfera.rotation.x = 0.5;
 // Posicionar la cámara
 camara.position.z = 5;
 
-// Animación del cilindro...
+// Animación
 (function animacion() {
 	requestAnimationFrame(animacion);
 	cilindro.rotation.x += 0.01;
@@ -36,10 +52,13 @@ camara.position.z = 5;
 	esfera.rotation.y += 0.01;
 	esfera.rotation.z += 0.01;
 
+	renderizador.autoClear = false;
+	renderizador.clear();
+	renderizador.render(backgroundScene, backgroundCamera);
 	renderizador.render(escena, camara);
 })();
 
-// Ajustes    
+// Ajustes
 window.addEventListener('resize', () => {
     camara.aspect = window.innerWidth / window.innerHeight;
     camara.updateProjectionMatrix();
